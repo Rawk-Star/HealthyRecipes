4de4e1c758b75384bf5d16e0ece8160e34cb1152
Rawk-Star <40251240+Rawk-Star@users.noreply.github.com>
2018-09-17 21:35:12 -0400

    Initial commit

---
+++ app/src/main/java/com/rawks/jeremy/healthyrecipes/MainActivity.java
@@ -0,0 +1,27 @@
+package com.rawks.jeremy.healthyrecipes;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.btnRecipe);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Recipe.class));
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/rawks/jeremy/healthyrecipes/Recipe.java
@@ -0,0 +1,13 @@
+package com.rawks.jeremy.healthyrecipes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Recipe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
+    }
+}
