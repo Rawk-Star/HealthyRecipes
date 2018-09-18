c489a73c7080a57c68720f8df48fcef62d251868
Rawk-Star <40251240+Rawk-Star@users.noreply.github.com>
2018-09-17 22:23:49 -0400

    Added some comments.

--- app/src/main/java/com/rawks/jeremy/healthyrecipes/MainActivity.java
+++ app/src/main/java/com/rawks/jeremy/healthyrecipes/MainActivity.java
@@ -8,20 +8,23 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button)findViewById(R.id.btnRecipe);
+        Button button = findViewById(R.id.btnRecipe);
+
+        // Create a listener to monitor the "View Recipe" button click event
         button.setOnClickListener(new View.OnClickListener() {
+
+            // This function is the event handler for when the user clicks the "View Recipe" button.
+            // It opens the recipe activity
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, Recipe.class));
             }
         });
-
     }
 }
